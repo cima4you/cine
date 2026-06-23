1cb8082b873adbb64a5336575d39a7bb7e75a51b
bazighe <bazighe@users.noreply.github.com>
2026-06-23 22:40:14 +0100

    إضافة PWA + Service Worker + Manifest

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'rcine-v1';
+const URLS = ['/', '/index.html', '/manifest.json', '/data-loader.js', '/logo.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const c = caches.open(CACHE);
+      c.then(cache => cache.put(e.request, res.clone()));
+      return res;
+    }))
+  );
+});
